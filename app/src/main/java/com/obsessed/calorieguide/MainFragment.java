a4490dc49086d82df6fbc14ea72f7c0a63cd0be0
Igorello <ioproglab@gmail.com>
2024-03-10 20:43:05 +0300

    refactor: added view binding, and arrow_back_24.xml

--- app/src/main/java/com/obsessed/calorieguide/MainFragment.java
+++ app/src/main/java/com/obsessed/calorieguide/MainFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -33,6 +35,10 @@ public class MainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        NavController navController = Navigation.findNavController(view);
 
+//        view.findViewById(R.id.arrow_back).setOnClickListener(v -> {
+//            navController.navigate(R.id.action_loginFragment_to_mainFragment);
+//        });
     }
 }
