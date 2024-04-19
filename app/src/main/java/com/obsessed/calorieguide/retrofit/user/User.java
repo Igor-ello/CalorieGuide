58ade5186c10eaef00038b559c6c404f65afcad5
Igorello <ioproglab@gmail.com>
2024-04-19 22:21:23 +0300

    feat: added visualising user params in profile

--- app/src/main/java/com/obsessed/calorieguide/retrofit/user/User.java
+++ app/src/main/java/com/obsessed/calorieguide/retrofit/user/User.java
@@ -20,24 +20,35 @@ public class User {
     public String getName() {
         return user_name;
     }
-
     public String getSurname() {
         return surname;
     }
-
     public String getEmail() {
         return email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public int getId() {
         return id;
     }
-
     public String getBearerToken() {
         return BearerToken;
+    }
+
+    public void setName(String name) {
+        this.user_name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
